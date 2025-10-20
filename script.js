8b9ca7e7bf2390b6dc77ea6e349c45cdc45bac90
sadiksadov <saidkarimsaidmuhammedov56@gmail.com>
2025-10-20 10:45:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+let raqam = 42;               
+let booly = true;            
+let bosh = null;         
+let notDefined;              
+let narsa = { name: "Alex" };  
+
+
+console.log(typeof str);        
+console.log(typeof raqam);       
+console.log(typeof booly);       
+console.log(typeof bosh);    
+console.log(typeof notDefined); 
+console.log(typeof narsa);
+
+
+
+const name = prompt("ismingni kiriting:");
+const age = Number(prompt("yoshingizni kiriting:"));
+
+console.log(`Привет, ${name}! biryildan sond senga  ${age + 1}.`);
+
+
+
+const num1 = Number(prompt("birinchis soni kiriting:"));
+const num2 = Number(prompt("ikinchi soni kiriting:"));
